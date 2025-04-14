ab07b2519023fefae7c883eca37f44d48442089a
khaakman <koenhaakman@gmail.com>
2025-04-14 13:26:31 +0200

    use GTSM annual csv instead of monthly

--- scripts/preprocess_GTSM.py
+++ scripts/preprocess_GTSM.py
@@ -4,36 +4,36 @@ import matplotlib.pyplot as plt
 
 
 ### OPTIONS ###
-station = 'Vlissingen'
+stations = ['Vlissingen', 'Hoek van Holland', 'IJmuiden', 'Den Helder', 'Harlingen', 'Delfzijl']
 years = np.arange(1890, 2022) #for which period you want to make correction
 ###############
 
-
 years_GTSM = np.arange(1950, 2023) #GTSM surge is from Jan 1950 until Dec 2022
-fn = '../data/GTSM/gtsm_surge_monthly_mean_main_stations_2023.csv'
+fn = '../data/GTSM/gtsm_surge_annual_mean_main_stations_2023.csv'
 data = pd.read_csv(fn) 
-data = data[data.name == station]
-surge = data.surge.to_numpy() * 100 #m to cm
-surge_yearly = np.zeros(len(years))
-mean_surge = np.mean(surge) #mean surge used to correct years before 1950
 
-for i in range(len(surge_yearly)):
-    if years[i] not in years_GTSM:
-        surge_yearly[i] = mean_surge
+for station in stations:
+    data_station = data[data.name == station]
+    surge = data_station.surge.to_numpy() * 100 #m to cm
+    extended_surge = np.zeros(len(years)) # empty array to store surge for chosen years
+    mean_surge = np.mean(surge) #mean surge used to correct years before 1950
+    
+    for i in range(len(extended_surge)):
+        if years[i] not in years_GTSM:
+            extended_surge[i] = mean_surge
+        else:
+            GTSM_idx = np.where(years_GTSM == years[i])[0][0]
+            extended_surge[i] = surge[GTSM_idx] #np.mean(surge[12*GTSM_idx:12*(GTSM_idx+1)])
+        
+    if station == 'Hoek van Holland':
+        savename = 'HvH'
+    elif station == 'Den Helder':
+        savename = 'DenHelder'
     else:
-        GTSM_idx = np.where(years_GTSM == years[i])[0][0]
-        surge_yearly[i] = np.mean(surge[12*GTSM_idx:12*(GTSM_idx+1)])
+        savename = station
+        
+    np.save('../processed_data/GTSM_surge_{}_yearly.npy'.format(savename), extended_surge)
     
-if station == 'Hoek van Holland':
-    savename = 'HvH'
-elif station == 'Den Helder':
-    savename = 'DenHelder'
-else:
-    savename = station
-    
-np.save('../processed_data/GTSM_surge_{}_yearly.npy'.format(savename), surge_yearly)
-
-plt.figure()
-plt.plot(years, surge_yearly)
-plt.ylabel('Height [cm]')
-
+    plt.figure()
+    plt.plot(years, extended_surge)
+    plt.ylabel('Height [cm]')
